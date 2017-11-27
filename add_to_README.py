1acfc3cb3f2384cbd22c6d48baf4db27ef308892
logangraham <logancgraham@gmail.com>
2017-11-27 17:16:48 +0000

    First commit

---
+++ add_to_README.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import glob
+import os
+import io
+import pytablewriter
+from datetime import date
+
+
+def add_to_markdown_README(paper_table_filepath):
+    df = pd.read_csv(paper_table_filepath)
+    df = df[['DATE', 'TITLE', 'AUTHOR', 'ABS_LINK', 'PDF_LINK']]
+    df.ABS_LINK = df.ABS_LINK.apply(lambda x: '[Abstract]({})'.format(x))
+    df.PDF_LINK = df.PDF_LINK.apply(lambda x: '[PDF]({})'.format(x))
+    df['Links'] = df.ABS_LINK + ' | ' + df.PDF_LINK
+    df = df.drop(['ABS_LINK', 'PDF_LINK'], axis=1)
+    df.columns = ['Date', 'Title', 'First Author', 'Links']
+    writer = pytablewriter.MarkdownTableWriter()
+    writer.stream = io.StringIO()
+    writer.header_list = list(df.columns)
+    writer.value_matrix = df.values
+    writer.write_table()
+    writer.stream.seek(0)
+    markdown = "".join(writer.stream.readlines()).encode('utf8')
+
+    current_date = date.today()
+    ymd = " / ".join(map(str, [current_date.year,
+                               current_date.month,
+                               current_date.day]))
+
+    final_string = "##{}\n".format(ymd) + markdown
+
+    with open("README.md", 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(final_string + '\n\n' + content)
+
+if __name__ == '__main__':
+    list_of_files = glob.glob('../weekly_pulls/*') # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    print latest_file
+    filepath = '../weekly_pulls/{}'.format(latest_file)
+    add_to_markdown_README(filepath)
---
+++ pull_weekly_update.py
@@ -0,0 +1,139 @@
+import urllib
+import feedparser
+import unicodecsv as csv
+import datetime
+
+
+def fetch_papers(start, max_results=10, only_recent=True):
+
+    # Base api query url
+    base_url = 'http://export.arxiv.org/api/query?'
+
+    # Search parameters
+    def list_to_OR_params(l, prefix, quotations=False):
+        """
+        This formats lists of strings to '(prefix:string1)+OR+(prefix:string2)...'
+        quotations=True puts quotations: '(prefix:"string1")+OR+(prefix:"string2")...'
+        """
+        if quotations:
+            l = map(lambda x: '"%s"' % x, l)
+        l = map(lambda x: '({}:'.format(prefix) + '+'.join(x.split()) + ')', l)
+        return "+OR+".join(l)
+
+    general_terms = [
+                     'causation',
+                     'cause',
+                     'causal',
+                     'causes'
+                    ]
+    general_abs = list_to_OR_params(general_terms, 'abs', quotations=False)
+    general_ti = list_to_OR_params(general_terms, 'ti', quotations=False)
+
+    specific_terms = ['do-calculus',
+                      'do calculus',
+                      'structural equation',
+                      'instrumental variable',
+                      'counterfactual'
+                     ]
+    specific_abs = list_to_OR_params(specific_terms, 'abs', quotations=True)
+    specific_ti = list_to_OR_params(specific_terms, 'ti', quotations=True)
+
+    category_list = ['cs.CV',
+                     'cs.AI',
+                     'cs.LG',
+                     'cs.CL',
+                     'cs.NE',
+                     'stat.ML'
+                    ]
+    categories = list_to_OR_params(category_list, 'cat')
+
+    search_query = '(%s)+AND+(%s+OR+%s+OR+%s+OR+%s)' % (categories,
+                                                        general_abs,
+                                                        general_ti,
+                                                        specific_abs,
+                                                        specific_ti)
+
+    query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
+                                                         start,
+                                                         max_results)
+
+    if only_recent:
+        suffix = "&sortBy=submittedDate&sortOrder=descending"
+        query += suffix
+
+    full_url = base_url + query
+    print full_url
+
+    feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
+    feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
+
+    response = urllib.urlopen(base_url+query).read()
+
+    feed = feedparser.parse(response)
+
+    print 'Feed title: %s' % feed.feed.title
+    print 'Feed last updated: %s' % feed.feed.updated
+
+    print 'totalResults for this query: %s' % feed.feed.opensearch_totalresults
+    print 'itemsPerPage for this query: %s' % feed.feed.opensearch_itemsperpage
+    print 'startIndex for this query: %s'   % feed.feed.opensearch_startindex
+
+    rows = []
+    for entry in feed.entries:  # extract information & add to list
+        entry_id = entry.id.split('/abs/')[-1]
+        date = datetime.datetime.strptime(str(entry.published)[:10], '%Y-%m-%d').date()
+        title = entry.title
+        first_author = entry.author
+        all_categories = ", ".join([t['term'] for t in entry.tags])
+        primary_category = entry.tags[0]['term']
+        abstract = entry.summary
+        for link in entry.links:
+            if link.rel == 'alternate':
+                abs_link = link.href
+            elif link.title == 'pdf':
+                pdf_link = link.href
+        row = [entry_id,
+               date,
+               title,
+               first_author,
+               all_categories,
+               primary_category,
+               abstract,
+               abs_link,
+               pdf_link]
+        rows.append(row)
+    return feed.feed.opensearch_totalresults, rows
+
+if __name__ == "__main__":
+    past_n_days = 7  # fetch papers from last n_days
+    num_papers = 100  # start by searching 100 at a time, due to API constraints
+
+    current_date = datetime.date.today()
+    ymd = "_".join(map(str, [current_date.year,
+                             current_date.month,
+                             current_date.day]))
+    filename = ymd + '_fetch.csv'
+
+    print("Searching...\n\n")  # fetch results
+    all_data = [["ID",
+                "DATE",
+                "TITLE",
+                "AUTHOR",
+                "CATEGORIES",
+                "FIRST_CATEGORY ",
+                "ABSTRACT",
+                "ABS_LINK",
+                "PDF_LINK"]]
+    num_results, _ = fetch_papers(0, 5)  # find total number of searches
+    for i in range(0, int(num_results) + 1, num_papers):  # iterate searches
+        _, r = fetch_papers(i, num_papers)
+        rows = filter(lambda x: (current_date - x[1]).days <= past_n_days, r)
+        all_data += rows
+        if len(rows) < len(r):
+            break  # break if there are entries older than past_n_days
+    print "Num papers: ", len(all_data)
+
+    filename = '../weekly_pulls/' + filename
+    with open(filename, "wb") as f:
+        writer = csv.writer(f, dialect='excel', encoding='utf-8')
+        writer.writerows(all_data)
